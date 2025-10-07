bf50047fa0ea735a80cd6a2157c32f072bcb3d45
Sandeep kumar <skverma895746@gmail.com>
2025-10-07 13:19:05 +0530

    Update Constructer.java

--- Constructer.java
+++ Constructer.java
@@ -2,21 +2,15 @@
 class  A {//default constructor
 
 int a=19,b=45,c=a+b;
-    
-
 A()
 {    
     System.out.println(a+"  "+b+"  "+c);
 }
-
 }
-
-
-
 public class Constructer {
     public static void main(String[]args)
     {
             A r=new A();
-
     }
 }
+
